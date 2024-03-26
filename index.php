9a94c3e797e939ad241b4d69eb4cd0f348f25b0e
Samar Ben Houidi <147640400+samar14bh@users.noreply.github.com>
2024-03-26 22:13:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php include'components/header.php' ?>
+    <!-- slider_area_start -->
+    <div class="slider_area">
+        <div class="single_slider  d-flex align-items-center slider_bg_1">
+            <div class="container">
+                <div class="row align-items-left justify-content-left" style="height:60px;">
+                    <div class="col-xl-8 ">
+                        <div class="slider_text text-center">
+                            <div class="text">
+                                <h3 style="color:white;text-align: left; ">
+                                    Find and share everyday<br> cooking inspiration on FoodHub
+                                </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <!-- slider_area_end -->
+    <!-- recepie_videos   -->
+    <div class="recepie_videoes_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-6">
+                    <div class="recepie_info">
+                        <h3>mouthwatering recipes and culinary inspiration!</h3>
+                
+                    <div class="video_watch d-flex align-items-center">
+                        <a href="about.php"> <i class="ti-arrow-right"></i> </a>
+                        <div class="watch_text" >
+                            <h4>About us </h4>
+                            <p>You will love our execution</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="videos_thumb">
+                        <div class="big_img">
+                            <img src="img/video/big.png" alt="">
+                        </div>
+                        <div class="small_thumb">
+                            <img src="img/video/small_1.png" alt="">
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ recepie_videos   -->
+    <!-- recepie_area_start  -->
+    <div class="recepie_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="single_recepie text-center">
+                        <div class="recepie_thumb">
+                            <img src="img/recepie/recpie_1.png" alt="">
+                        </div>
+                        <h3>Egg Manchurian</h3>
+                        <span>Appetizer</span>
+                        <p>Time Needs: 30 Mins</p>
+                        <a href="#" class="line_btn">View Full Recipe</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="single_recepie text-center">
+                        <div class="recepie_thumb">
+                            <img src="img/recepie/recpie_2.png" alt="">
+                        </div>
+                        <h3>Pure Vegetable Bowl</h3>
+                        <span>Appetizer</span>
+                        <p>Time Needs: 30 Mins</p>
+                        <a href="#" class="line_btn">View Full Recipe</a>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6">
+                    <div class="single_recepie text-center">
+                        <div class="recepie_thumb">
+                            <img src="img/recepie/recpie_3.png" alt="">
+                        </div>
+                        <h3>Egg Masala Ramen</h3>
+                        <span>Appetizer</span>
+                        <p>Time Needs: 30 Mins</p>
+                        <a href="#" class="line_btn">View Full Recipe</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /recepie_area_start  -->
+
+    
+
+    <div class="latest_trand_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="trand_info text-center">
+                       
+                        <h3>Discover latest trending recipes</h3>
+                        <a href="#" class="boxed-btn3">View all Recipes</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ latest_trand     -->
+
+   
+
+    <?php include 'components/footer.php'?>
+
+   
