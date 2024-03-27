38eaf453bc9178c163b0d64028b82dbab0f8431d
Samar Ben Houidi <147640400+samar14bh@users.noreply.github.com>
2024-03-27 15:46:57 +0100

    Add files via upload

--- recipes_details.php
+++ recipes_details.php
@@ -1,4 +1,17 @@
-<?php include 'components/header.php'?>
+<?php 
+ require_once('components/autoload.php');
+ $repository = new RecipeRepository();
+ require_once('components/header.php');
+ if(isset($_GET['recipe'])){
+
+    $recipeId = (int)$_GET['recipe'];
+    $recipe=$repository->findById($recipeId);
+ }else{
+    $recipeId = '';
+    header('location:recipes.php');
+ }
+ 
+?>
     <div class="bradcam_area bradcam_bg_1">
         <div class="container">
             <div class="row">
@@ -13,47 +26,77 @@
     <!-- /bradcam_area  -->
 
     <div class="recepie_details_area">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-xl-6 col-md-6">
-                    <div class="recepies_thumb">
-                        <img src="img/recepie/recepie_details.png" alt="">
-                    </div>
-                </div>
-                <div class="col-xl-6 col-md-6">
-                    <div class="recepies_info">
-                        <h3>Dragon tiger phoenix</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
 
-                        <div class="resepies_details">
-                            <ul>
-                                <li><p><strong>Rating</strong> : <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i> </p></li>
-                                <li><p><strong>Time</strong> : 30 Mins </p></li>
-                                <li><p><strong>Category</strong> : Main Dish </p></li>
-                                <li><p><strong>Tags</strong> :  Dinner, Main, Chicken, Dragon, Phoenix </p></li>
-                            </ul>
+
+        <?php  if(!empty($recipe)){ ?>
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-6 col-md-6">
+                            <div class="recepies_thumb">
+                            <img src="../img/recepie/<?=$recipe->Image?>" alt="food">
+                            </div>
                         </div>
-                        <div class="links">
-                            <a href="#"> <i class="fa fa-facebook"></i> </a>
-                            <a href="#"> <i class="fa fa-twitter"></i> </a>
-                            <a href="#"> <i class="fa fa-linkedin"></i> </a>
+                        <div class="col-xl-6 col-md-6">
+                            <div class="recepies_info">
+                                <h3><?=$recipe->Name;?></h3>
+                                
+
+                                <div class="resepies_details">
+                                    <ul>
+                                        <li>
+                                            <p><strong>Rating</strong> : 
+                                            <?php 
+                                                $averageRating = $recipe->AverageRating;
+                                                for ($i = 1; $i <= 5; $i++) {
+                                                    if ($i <= $averageRating) {
+                                                        echo '<i class="fa fa-star"></i>';
+                                                    } else {
+                                                        echo '<i class="fa fa-star-o"></i>';
+                                                    }
+                                                }
+                                            ?>
+                                            </p>
+                                        </li>
+                                     </p></li>
+                                        <li><p><strong>Time</strong> : <?=$recipe->Time?> Mins </p></li>
+                                        <li><p><strong>Category</strong> : <?=$recipe->Type?> </p></li>
+                                        <h3 style="margin-top:15px;">Ingredients</h3>
+                                        <p><?= $recipe->Ingredients?> </P>
+      
+                                    </li>
+                                        
+                                    </ul>
+                                </div>
+                                <div class="links">
+                                    <a href="#"> <i class="fa fa-facebook"></i> </a>
+                                    <a href="#"> <i class="fa fa-twitter"></i> </a>
+                                    <a href="#"> <i class="fa fa-linkedin"></i> </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="recepies_text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
-                        <p> Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit. Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="recepies_text">
+                                <p><?= $recipe->Description?></p>
+                                
+                            </div>
+                            </div>
+                            <div class="form-group mt-3">
+                        <a href="review.php?recipe=idRecipe" class="button button-contactForm btn_4 boxed-btn">Write a Review</a>
+                        </div>
+                        <div class="form-group mt-3">
+                        <a href="generatePdf.php?recipe=<?= $recipe->Id; ?>" class="button button-contactForm btn_4 boxed-btn" style="margin-left:30px;"><i class="fa fa-download"></i> Download PDF</a>
+                        </div>
+                        
                     </div>
-                    <div class="form-group mt-3">
-                <a href="review.php?recipe=idRecipe" class="button button-contactForm btn_4 boxed-btn">Write a Review</a>
-            </div>
                 </div>
-            </div>
-        </div>
+
+
+
+        <?php }else{ ?>
+            <h2>No recipe found</h2>
+        <?php } ?>
     </div>
     <!-- recepie_area_start  -->
     <div class="recepie_area inc_padding">
@@ -97,7 +140,154 @@
     </div>
     <!-- /recepie_area_start  -->
 
- 
+    <!-- footer  -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                Top Products
+                            </h3>
+                            <ul>
+                                <li><a href="#">Managed Website</a></li>
+                                <li><a href="#"> Manage Reputation</a></li>
+                                <li><a href="#">Power Tools</a></li>
+                                <li><a href="#">Marketing Service</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                Quick Links
+                            </h3>
+                            <ul>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Brand Assets</a></li>
+                                <li><a href="#">Investor Relations</a></li>
+                                <li><a href="#">Terms of Service</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                Features
+                            </h3>
+                            <ul>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Brand Assets</a></li>
+                                <li><a href="#">Investor Relations</a></li>
+                                <li><a href="#">Terms of Service</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                Resources
+                            </h3>
+                            <ul>
+                                <li><a href="#">Guides</a></li>
+                                <li><a href="#">Research</a></li>
+                                <li><a href="#">Experts</a></li>
+                                <li><a href="#">Agencies</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6 col-lg-4">
+                            <div class="footer_widget">
+                                    <h3 class="footer_title">
+                                            Subscribe
+                                    </h3>
+                                    <p class="newsletter_text">You can trust us. we only send promo offers,</p>
+                                    <form action="#" class="newsletter_form">
+                                        <input type="text" placeholder="Enter your mail">
+                                        <button type="submit"> <i class="ti-arrow-right"></i> </button>
+                                    </form>
+                                    
+                                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right_text">
+            <div class="container">
+                <div class="footer_border"></div>
+                <div class="row align-items-center">
+                    <div class="col-xl-8 col-md-8">
+                        <p class="copy_right">
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                    <div class="col-xl-4 col-md-4">
+                        <div class="socail_links">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <i class="ti-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="ti-twitter-alt"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dribbble"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-behance"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+<!--/ footer  -->
+
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/gijgo.min.js"></script>
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+    <script src="js/main.js"></script>
 </body>
-<?php include 'components/footer.php'?>
+
 </html>
