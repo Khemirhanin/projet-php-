ff6bad525c9ecbf12cabb0ee4c2a55f25bbca5be
1EyaBA <147639959+1EyaBA@users.noreply.github.com>
2024-03-30 07:46:22 +0100

    dashboard layout
    the statistics page without php

---
+++ statistics.php
@@ -0,0 +1,64 @@
+<!-- the header -->
+<?php include_once "components/adminHeader.php"; ?>
+    <!-- slider_area_start -->
+
+  <div class="contain">
+    
+    <div class="statistics">
+    <div class="defaultStyle connectedUsers ">
+          <h2>Connected Users</h2>
+          <br>
+          <h4>5</h4>
+        </div>
+        <div class="defaultStyle totalUsers">
+          <h2>Total Users</h2>
+          <br>
+          <h4>20</h4>
+          
+        </div>
+        <div class="defaultStyle categories">
+          <h2>Categories</h2>
+          <br>
+          <h4>3</h4>
+          
+        </div>
+        <div class="defaultStyle totalRecipes">
+          <h2>Recipes</h2>
+          <br>
+          <h4>10</h4>
+          
+
+        </div>
+        <div class="defaultStyle totalRecipes">
+          <h2>Downloads</h2>
+          <br>
+          <h4>10</h4>
+          
+
+        </div>
+        <div class="defaultStyle totalRecipes">
+          <h2>Requests</h2>
+          <br>
+          <h4>4</h4>
+          
+
+        </div>
+        
+
+    </div>
+  </div>
+     
+
+
+
+
+
+
+
+
+
+
+<!-- footer  -->
+<?php
+include_once "components/footer.php";
+?>
