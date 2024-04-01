1f4dcf5cf86e00a41aa93aa55ea9246401c1b936
Emna Gharbi <135608321+EmGh3@users.noreply.github.com>
2024-04-01 23:18:38 +0100

    update recipeRepository

--- components/RecipeRepository.php
+++ components/RecipeRepository.php
@@ -4,6 +4,33 @@
         public function __construct() {
             parent::__construct("recipes");
         }
+        public function updateAverageRating($rating,$recipeID) {
+            $query = "update recipes set AverageRating = ? where Id = ?";
+            $ps = $this->db->prepare($query);
+            $ps->execute([$rating,$recipeID]); 
+        }
+        public function findApprovedRecipes(){
+            $query = "select * from recipes where confirm is true";
+            $response = $this->db->query($query);
+            $recipes = $response->fetchAll(PDO::FETCH_OBJ);
+            return $recipes;  
+        }
+        public function findRequests(){
+            $query = "select * from recipes where confirm is false";
+            $response = $this->db->query($query);
+            $recipes = $response->fetchAll(PDO::FETCH_OBJ);
+            return $recipes;  
+        }
+        public function acceptRecipe($recipeID){
+            $query = "update recipes set confirm = true where Id = ?";
+            $ps = $this->db->prepare($query);
+            $ps->execute([$recipeID]); 
+        }
+        public function deleteRecipe($recipeID){
+            $query = "delete from recipes where Id = ?";
+            $ps = $this->db->prepare($query);
+            $ps->execute([$recipeID]); 
+        }
     }
     
 ?>
